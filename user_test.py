c80911bdf61564d770187ff6d2f7cc35f8878335
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-30 22:16:44 +0300

    Adding testing part for new details of users are saved

--- user_test.py
+++ user_test.py
@@ -23,6 +23,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_users.first_name, 'Dennis')
         self.assertEqual(self.new_users.last_name, 'Kiplangat')
         self.assertEqual(self.new_users.password, 'kiplangat18')
+    
+    def test_save_user(self):
+        '''
+		Test to check if the new users information is saved into the users list
+		'''
+        self.new_users.save_user()
+        self.assertEqual(len(User.users_list), 1)
 
 
 if __name__ == '__main__':
