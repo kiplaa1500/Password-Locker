55d3b50ef7bf39228a9888eac523f8ee25379ef9
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-30 23:29:36 +0300

    Adding function to confirm password, to gen password and create new user details

--- run.py
+++ run.py
@@ -16,3 +16,28 @@ def save_user(user):
 	Function to save a new user account
 	'''
 	User.save_user(user)
+ 
+ 
+ 
+def confirm_user(first_name, password):
+	'''
+	Function that confirms the existance of the user before creating details 
+	'''
+	checking_user = Details.check_user(first_name, password)
+	return checking_user
+
+
+def generate_password():
+	'''
+	Function to generate a password for the user.
+	'''
+	gen_pass = Details.generate_password()
+	return gen_pass
+
+
+def create_details(user_name, site_name, account_name, password):
+	'''
+	Function to create a new details.
+	'''
+	new_details = Details(user_name, site_name, account_name, password)
+	return new_details
