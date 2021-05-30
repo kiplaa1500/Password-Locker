a56f677c953962091aeb5b4a78f020f1f0d2ea12
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-30 21:27:10 +0300

    Adding function to save newly created instances

--- user_details.py
+++ user_details.py
@@ -20,4 +20,10 @@ class User:
      self.first_name = first_name
      self.last_name = last_name
      self.password = password
+     
+    def save_user(self):
+        """
+        Function to save a newly created instances.
+        """
+        User.users_list.append(self)
     
