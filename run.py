94636d0aa0c2813f570e2fc51c98a80bce70249f
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-31 10:32:47 +0300

    Adding main funtion that renders every step required

--- run.py
+++ run.py
@@ -62,3 +62,99 @@ def copy_details(site_name):
 	Function to copy a details details to the clipboard
 	'''
 	return Details.copy_details(site_name)
+
+
+def main():
+	print(' ')
+	print('Welcome to Password Locker application.')
+	while True:
+		print('Use the codes to navigate: \n na-Create an Account \n li-Log In \n ex-Exit')
+		short_code = input('Enter a choice: ').lower().strip()
+		if short_code == 'ex':
+			break
+
+		elif short_code == 'na':
+			print('To create a new account:')
+			first_name = input('Enter your first name - ').strip()
+			last_name = input('Enter your last name - ').strip()
+			password = input('Enter your password - ').strip()
+			save_user(create_user(first_name, last_name, password))
+			print(" ")
+			print(
+				f'New Account Created for: {first_name} {last_name} using password: {password}')
+		elif short_code == 'li':
+			print(' ')
+			print('To login, enter your account details:')
+			user_name = input('Enter your first name - ').strip()
+			password = str(input('Enter your password - '))
+			user_exists = confirm_user(user_name, password)
+			if user_exists == user_name:
+				print(" ")
+				print(f'Welcome {user_name}. Please choose an option to continue.')
+				print(' ')
+				while True:
+					print('Navigation codes: \n cc-Create a Details \n dc-Display Details \n copy-Copy Password \n ex-Exit')
+					short_code = input('Enter a choice: ').lower().strip()
+					if short_code == 'ex':
+						print(" ")
+						print(f'Goodbye {user_name}')
+						break
+					elif short_code == 'cc':
+						print(' ')
+						print('Enter your details details:')
+						site_name = input('Enter the site\'s name- ').strip()
+						account_name = input('Enter your account\'s name - ').strip()
+						while True:
+							print(' ')
+							print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
+							psw_choice = input('Enter an option: ').lower().strip()
+							if psw_choice == 'ep':
+								print(" ")
+								password = input('Enter your password: ').strip()
+								break
+							elif psw_choice == 'gp':
+								password = generate_password()
+								break
+							elif psw_choice == 'ex':
+								break
+							else:
+								print('Wrong option entered. Try again.')
+						save_details(create_details(
+							user_name, site_name, account_name, password))
+						print(' ')
+						print(
+							f'details Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
+						print(' ')
+					elif short_code == 'dc':
+						print(' ')
+						if display_details(user_name):
+							print('Here is a list of all your details')
+							print(' ')
+							for details in display_details(user_name):
+								print(
+									f'Site Name: {details.site_name} - Account Name: {details.account_name} - Password: {details.password}')
+							print(' ')
+						else:
+							print(' ')
+							print("You don't seem to have any details saved yet")
+							print(' ')
+					elif short_code == 'copy':
+						print(' ')
+						chosen_site = input(
+							'Enter the site name for the details password to copy: ')
+						copy_details(chosen_site)
+						print('')
+					else:
+						print('Wrong option entered. Try again.')
+
+			else:
+				print(' ')
+				print('Wrong details entered. Try again or Create an Account.')
+
+		else:
+			print(' ')
+			print('Wrong option entered. Try again.')
+   
+
+if __name__ == '__main__':
+	main()
